c76aefa8b8a3cbd8071165f419fa18d3e9d3aef8
Kavyanjali <kvyanjali@gmail.com>
2026-04-06 08:43:39 +0530

    Display Hello with multiple args using enhanced for loop

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,13 +1,19 @@
-public class HelloApp{
-    
-public static void main(String[] args) {
-  
-    if (args.length > 0) {
-        String name = args[0];
-        System.out.println("Hello, " + name + "!");
-    } else {
-        System.out.println("Hello, World!");
+public class HelloApp {
+
+    public static void main(String[] args) {
+		StringBuilder nameBuilder = new StringBuilder();
+		boolean first = true;
+		for (String name : args) {
+			if (!first) {
+				nameBuilder.append(", ");
+			}
+			nameBuilder.append(name);
+			first = false;
+		
+		} 
+		String name = nameBuilder.toString();
+        System.out.println("Hello " + name + "!");
     }
-    }
-    }
+
+}
        
